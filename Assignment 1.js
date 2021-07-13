a2ff2b32b88b8366a748c27c82138115fb7a6caa
SrushtiWakchaure <87388738+SrushtiWakchaure@users.noreply.github.com>
2021-07-13 23:46:49 +0530

    Add files via upload

---
+++ Assignment 1.js
@@ -0,0 +1,13 @@
+// Day 2-Javascript Essentials
+// Printing 5"*" Horizontally
+
+for(i=0; i<=5; i=i+1) {
+   v= ""
+  
+   for(j=0; j<=4; j=j+1){
+     v= v+" *"
+   }
+ }
+ console.log(v)
+
+//output * * * * * 
